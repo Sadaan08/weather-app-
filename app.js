e8d3f4009a97ff1c91092af23fb0580b3378d4d7
o md sadaan <omdsadaan@gmail.com>
2024-08-04 17:08:42 +0530

    Add files via upload
    weather web application

---
+++ app.js
@@ -0,0 +1,25 @@
+const apikey = "be3569fb1c11943a858137ffebfc9f19";
+
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkweather(city) {
+    const response = await fetch(apiurl + city + `&appid=${apikey}`);
+    var data = await response.json();
+
+    console.log(data)
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/hr";
+
+document.querySelector(".weather").style.display = "block";
+}
+
+searchbtn.addEventListener("click", () => {
+    checkweather(searchbox.value);
+
+});
